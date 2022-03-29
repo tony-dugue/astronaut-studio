ab6e64483cc0fff57893f495fa0a9ab96ea6737f
tony dugué <duguetony@gmail.com>
2022-03-30 01:06:20 +0200

    section About => add left bloc (text + animation on title)

--- src/sections/About.js
+++ src/sections/About.js
@@ -3,10 +3,24 @@ import styled from 'styled-components'
 
 const About = () => {
   return (
-    <AboutWrapper>
-      <Title data-scroll data-scroll-speed="-2" data-scroll-direction="horizontal">
-        A Propos
-      </Title>
+    <AboutWrapper id="fixed-target">
+
+      <Title data-scroll data-scroll-speed="-2" data-scroll-direction="horizontal">A Propos</Title>
+
+      <Left data-scroll data-scroll-sticky data-scroll-target="#fixed-target">
+        Astronaut Studio est un studio créatif basé à Paris.
+        Animés par une équipe d'artistes, nous créons des oeuvres futuristes sur le thème de l'espace.
+        <br /><br />
+        Nous nous efforçons de bâtir sur notre vision. En tant que studio créatif, nous faisons notre maximum pour créer des expériences incroyables accessible à tous.
+        Travaillant avec plus de 50 artistes du monde entier, notre équipe de créateurs donne vie aux projets les plus futuristes.
+        Tout ce que nous pouvons rêver, nous pouvons le créer.
+        <br /><br />
+        Si le style et les envies de notre marque ont suffisamment éveillé votre curiosité, embarquez avec nous à bord de notre navette spatiale
+        pour découvrir plus précisément nos créations.
+      </Left>
+
+      <Right>Images</Right>
+
     </AboutWrapper>
   )
 }
@@ -16,12 +30,16 @@ export default About
 const AboutWrapper = styled.section`
   position: relative;
   width: 80vw;
-  min-height: 100vh;
+  min-height: 200vh;
   overflow: hidden;
+  
+  display: flex;
+  margin: 0 auto;
 `
 
 const Title = styled.h1`
   font-size: ${props => props.theme.fontBig};
+  color: ${props => props.theme.primary};
   font-family: "Kaushan Script";
   font-weight: 300;
   
@@ -29,5 +47,18 @@ const Title = styled.h1`
   top: 1rem;
   left: 5%;
   z-index: 5;
+`
+
+const Left = styled.div`
+  width: 50%;
+  font-size: ${props => props.theme.fontlg};
+  font-weight: 300;
+  position: relative;
+  z-index: 5;
+  margin-top: 24%;
+`
+
+const Right = styled.div`
+  width: 50%;
 `
 
